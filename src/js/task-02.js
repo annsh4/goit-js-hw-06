2df77b2d8c1ce6d05fb2d7c7ce70523c5ad48f20
Anna Tereshchuk <annaterr013@gmail.com>
2023-03-21 23:11:51 +0000

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const ingredientsItemsRef = ingredients.map((ingredient) => {
+  const ingredientsItem = document.createElement('li');
+  ingredientsItem.textContent = ingredient;
+  ingredientsItem.classList.add('item');
+  return ingredientsItem;
+});
+
+ingredientsRef.append(...ingredientsItemsRef);
