ba893b7964d7ce9b99296da4dd3c8a6a776915d7
Anna Tereshchuk <annaterr013@gmail.com>
2023-03-22 02:36:53 +0000

    task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const textColorRef = document.querySelector(".color");
+const changeColorBtnRef = document.querySelector(".change-color");
+const bodyRef = document.body;
+
+const changeColor = () => {
+  textColorRef.textContent = getRandomHexColor();
+  bodyRef.style.backgroundColor = textColorRef.textContent;
+};
+
+changeColorBtnRef.addEventListener("click", changeColor);
