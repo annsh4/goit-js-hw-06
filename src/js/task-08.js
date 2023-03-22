00b035ca310af9a2d7e2a00cf28837753f37e3ff
Anna Tereshchuk <annaterr013@gmail.com>
2023-03-22 02:30:16 +0000

    task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener('submit', submitForm);
+const profile = {};
+
+function submitForm(event) {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email;
+  const password = event.currentTarget.elements.password;
+
+  if (email.value === "" || password.value === "") {
+    alert("You must fill in every field");
+  } else {
+    profile.email = email.value;
+    profile.password = password.value;
+
+    console.log(profile);
+  }
+
+  event.currentTarget.reset();
+}
